8b81cdc1e41791c53a162b1b5c954ee9454c4558
jshea2 <joe.daniel.shea@gmail.com>
2022-04-03 12:55:12 -0700

    commit on 'master'

---
+++ src/main/main.ts
@@ -0,0 +1,547 @@
+/* eslint global-require: off, no-console: off, promise/always-return: off */
+
+/**
+ * This module executes inside of electron's main process. You can start
+ * electron renderer process from here and communicate with the other processes
+ * through IPC.
+ *
+ * When running `npm run build` or `npm run build:main`, this file is compiled to
+ * `./src/main.js` using webpack. This gives us some performance wins.
+ */
+import path from 'path';
+import { app, BrowserWindow, shell, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
+import MenuBuilder from './menu';
+import { resolveHtmlPath } from './util';
+const fs = require('fs')
+const { Client, Server } = require('node-osc');
+const net = require('net')
+const socket = new net.Socket()
+
+
+
+// export default class AppUpdater {
+//   constructor() {
+//     log.transports.file.level = 'info';
+//     autoUpdater.logger = log;
+//     autoUpdater.checkForUpdatesAndNotify();
+//   }
+// }
+
+let mainWindow: BrowserWindow | null = null;
+
+// ipcMain.on('ipc-example', async (event, arg) => {
+//   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
+//   console.log(msgTemplate(arg));
+//   event.reply('ipc-example', msgTemplate('pong'));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  const sourceMapSupport = require('source-map-support');
+  sourceMapSupport.install();
+}
+
+// const isDevelopment =
+//   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+
+// if (isDevelopment) {
+//   require('electron-debug')();
+// }
+
+// const installExtensions = async () => {
+//   const installer = require('electron-devtools-installer');
+//   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+//   const extensions = ['REACT_DEVELOPER_TOOLS'];
+
+//   return installer
+//     .default(
+//       extensions.map((name) => installer[name]),
+//       forceDownload
+//     )
+//     .catch(console.log);
+// };
+
+const windowWidth = 160;
+const windowHeight = 387;
+
+const createWindow = async () => {
+  // if (isDevelopment) {
+  //   await installExtensions();
+  // }
+
+  // ipcMain.on('ping', (event, arg) => {
+  //   console.log(arg)
+  //   mainWindow.webContents.send('test', "HIIIIIII")
+  // })
+
+  const RESOURCES_PATH = app.isPackaged
+    ? path.join(process.resourcesPath, 'assets')
+    : path.join(__dirname, '../../assets');
+
+  const getAssetPath = (...paths: string[]): string => {
+    return path.join(RESOURCES_PATH, ...paths);
+  };
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: windowWidth,
+    height: windowHeight,
+    autoHideMenuBar: true,
+    backgroundColor: '#081421', // background color
+    icon: getAssetPath('icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+  });
+
+  mainWindow.loadURL(resolveHtmlPath('index.html'));
+
+  mainWindow.on('ready-to-show', () => {
+    if (!mainWindow) {
+      throw new Error('"mainWindow" is not defined');
+    }
+    if (process.env.START_MINIMIZED) {
+      mainWindow.minimize();
+    } else {
+      mainWindow.show();
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+
+  // Open urls in the user's browser
+  mainWindow.webContents.setWindowOpenHandler((edata) => {
+    shell.openExternal(edata.url);
+    return { action: 'deny' };
+  });
+
+  // Remove this if your app does not use auto updates
+  // eslint-disable-next-line
+ // new AppUpdater();
+
+  let oscIpIn
+  let oscPortIn
+  let watchoutIpOut
+  let watchoutPortOut
+
+
+  // Log to Browser Console
+  function logEverywhere(message) {
+    if (mainWindow && mainWindow.webContents) {
+    mainWindow.webContents.executeJavaScript(`console.log(\`${message}\`)`);
+    }
+    }
+
+// Path To Data
+  const dataPath =
+  process.env.NODE_ENV === 'development'
+    ? path.join(__dirname, '../../data')
+    : path.join(process.resourcesPath, 'data');
+
+  //Read data from config.JSON file and handle data to renderer
+  const getConfig = () => {
+    const path1 = path.join(dataPath, 'config.json')
+    fs.readFile(path1, null, (_, data) => {
+      const jsonData = JSON.parse(data)
+      oscIpIn = jsonData.iposc
+      oscPortIn = jsonData.portosc
+      watchoutIpOut = jsonData.ipwatchout
+      watchoutPortOut = jsonData.portwatchout
+      console.log(`i got the config file:`)
+      console.log(jsonData)
+      ipcMain.handle('configDefaults', async (_,message) => {
+        return jsonData
+      })
+    })
+  }
+  getConfig()
+
+// Write data to config.JSON file
+  const saveData = (config) => {
+    const finished = (error) => {
+      if(error){
+        console.log(error);
+        return;
+      }
+    }
+    const jsonData = JSON.stringify(config, null, 2)
+    fs.writeFile(path.join(dataPath, 'config.json'), jsonData, finished);
+    console.log("saved file")
+  }
+
+  //Submit and Config Handle
+  ipcMain.handle('config', async (_, message) => {
+    console.log(message)
+    saveData(message)
+    oscIpIn = message.iposc
+    oscPortIn = message.portosc
+    watchoutIpOut = message.ipwatchout
+    watchoutPortOut = message.portwatchout
+    mainWindow.setSize((windowWidth*2+100),windowHeight)
+    mainWindow.webContents.openDevTools();
+    //logEverywhere("")
+
+    socket.connect({
+      host: watchoutIpOut,
+      port: watchoutPortOut,
+      readable: true,
+      writeable: true,
+    }, ()=>{
+      console.log("HELOOOOO")
+      logEverywhere(`[WATCHOUT] PING`)
+        socket.write(`ping\n`)
+        console.log(``)
+        setTimeout(() => {
+          socket.destroy()
+        }, 100);
+      }
+    )
+
+    socket.on('data', (data) => {
+      console.log(data)
+      console.log(data)
+      let b = Buffer.from(data, 'utf8')
+      console.log(JSON.stringify(b))
+      let isReady = b.toString('utf8')
+      if (isReady.includes("Ready")){
+        let arr = isReady.split(' ')
+        console.log(arr)
+        logEverywhere(`[PING] [WATCHOUT IS CONNECTED!]`)
+        logEverywhere(`[Version: ${arr[1]}]`)
+        mainWindow.webContents.send("woconnected", true)
+      }
+      if (isReady.includes("true")){
+        logEverywhere("[License is Active]")
+      } else if (isReady.includes("false")){
+        logEverywhere("[No License Found - DEMO Mode]")
+      }
+      //logEverywhere(isReady)
+    })
+
+    socket.on('error', (data) => {
+      console.log(data);
+      mainWindow.webContents.send("woconnected", false)
+      logEverywhere(`[WATCHOUT IS DISCONNECTED]`)
+    })
+
+    //Connect OSC
+    var oscServer = new Server(oscPortIn, oscIpIn, () => {
+      console.log('OSC Server is listening');
+      console.log(`OSC IP: ${oscIpIn}\n OSC Port: ${oscPortIn}`)
+
+
+    });
+
+    oscServer.on('bundle', function (bundle) {
+      bundle.elements.forEach((element, i) => {
+        console.log(`Timestamp: ${bundle.timetag[i]}`);
+        console.log(`Message: ${element}`);
+      });
+      //oscServer.close();
+    });
+
+    oscServer.on('message', function (msg) {
+
+
+
+
+        console.log(`Message: ${msg}`);
+        let msgArray = msg[0].split("/")
+        console.log(msgArray)
+        if(msgArray[1] == "watchout"){
+          let cueNumber = msgArray[3]
+          let command = msgArray[2]
+          console.log(`${watchoutIpOut} | ${watchoutPortOut}`)
+          socket.setTimeout(1000)
+          socket.on('connect', () => {
+            console.log('connect')
+          })
+          socket.on('data', (data) => {
+            console.log(data)
+            console.log(data)
+            let b = Buffer.from(data, 'utf8')
+            console.log(JSON.stringify(b))
+            let isReady = b.toString('utf8')
+            if (isReady.includes("Ready")){
+              logEverywhere("[PING] [WATCHOUT IS CONNECTED!]")
+              mainWindow.webContents.send("woconnected", true)
+            }
+            logEverywhere(isReady)
+          })
+          socket.on('close', ()=>{
+            console.log('closed')
+          })
+          socket.on('error', (err)=>{
+            console.log(err)
+            logEverywhere(err)
+          })
+
+          switch (command) {
+            case "go": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                console.log("HELOOOOO")
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                logEverywhere(`[WATCHOUT] GO TO CUE ${cueNumber} AND RUN`)
+                  socket.write(`gotoControlCue ${cueNumber}\nrun\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "run": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                console.log("HELOOOOO")
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                logEverywhere(`[WATCHOUT] RUN`)
+                  socket.write(`run\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "goto": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                console.log("HELOOOOO")
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                logEverywhere(`[WATCHOUT] GO TO CUE ${cueNumber} AND HALT`)
+                  socket.write(`gotoControlCue ${cueNumber}\nhalt\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "halt": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] HALT`)
+                  socket.write(`halt\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "online": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] ONLINE`)
+                  socket.write(`online true\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "offline": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] OFFLINE`)
+                  socket.write(`online false\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "reset": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] RESET`)
+                  socket.write(`reset\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "kill": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] KILL`)
+                  socket.write(`kill ${msg[1]}\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "standBy": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] STANDBY`)
+                  socket.write(`standBy true\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "load": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] LOAD`)
+                  socket.write(`load ${msg[1]}\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 1);
+                }
+              )
+            }
+            break
+            case "ping": {
+              socket.connect({
+                host: watchoutIpOut,
+                port: watchoutPortOut,
+                readable: true,
+                writeable: true,
+              }, ()=>{
+                logEverywhere(`[OSC IN] ${msg[0]}`)
+                console.log("HELOOOOO")
+                logEverywhere(`[WATCHOUT] PING`)
+                  socket.write(`ping\n`)
+                  console.log(``)
+                  setTimeout(() => {
+                    socket.destroy()
+                  }, 100);
+                }
+              )
+            }
+
+          }
+        } else if (msg[0].includes("/eos/out/event/")) {
+          logEverywhere(`[OSC IN] ${msg[0]}`)
+          //logEverywhere(`Invalid OSC Command... Try the following commands "/watchout/go/1", "/run", "/halt, "watchout/goto/2"`)
+        } else if (msg[0].includes("/eos/out/")){
+          return
+        } else {
+          logEverywhere(`[OSC IN] ${msg[0]}`)
+        }
+      })
+    //
+
+    return
+  })
+
+  ipcMain.handle('consoleWindow', async (_, message) => {
+    console.log("I GOT THE ARROW THING")
+    console.log(message)
+    if (message == false){
+      mainWindow.webContents.openDevTools()
+      mainWindow.setSize(windowWidth*2+100,windowHeight)
+    } else if (message == true){
+      mainWindow.webContents.closeDevTools()
+      mainWindow.setSize(windowWidth,windowHeight)
+    }
+    return "Hi there"
+  })
+
+};
+
+/**
+ * Add event listeners...
+ */
+
+app.on('window-all-closed', () => {
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (mainWindow === null) createWindow();
+    });
+  })
+  .catch(console.log);
---
+++ src/renderer/App.tsx
@@ -0,0 +1,221 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+import { useState } from 'react'
+import React from 'react'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import RecipeReviewCard from './ConsoleCollapse';
+let packageJson = require("../../package.json");
+
+let isRunOnce1 = true
+
+
+let iposc
+let portosc
+let ipwatchout
+let portwatchout
+let buttonConnected
+let textConnected = "Connect"
+
+const Hello = () => {
+ //console.log("New Render!")
+
+let [oscIpIn, setOscIpIn] = useState("");
+let [oscPortIn, setOscPortIn] = useState("");
+let [watchoutIpOut, setWatchoutIpOut] = useState("");
+let [watchoutPortOut, setWatchoutPortOut] = useState("");
+let [connected, setConnected] = useState("")
+let [_, setTextConnect] = useState("Connect")
+
+// const openDev = () => {
+//   window.electron.console("Hi")
+// }
+
+//window.electron.console("hiiiii")
+
+// iposc = oscIpIn
+// portosc = oscPortIn
+// ipwatchout = watchoutIpOut
+// portwatchout = watchoutPortOut
+
+window.electron.ipcRenderer.on('woconnected', (event, arg) => {
+  //console.log("App on happend")
+  //console.log(arg)
+  if (arg == true){
+    setConnected(buttonConnected = "success")
+  setTextConnect(textConnected = "Connected")
+} else {
+  setConnected(buttonConnected = "error")
+  setTextConnect(textConnected = "Disconnected")
+  }
+  console.log(`OSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nWatchout (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
+})
+
+const getConfigDefaults = async () => {
+  const result = await electron.getConfig()
+  iposc = result.iposc
+  portosc = result.portosc
+  ipwatchout = result.ipwatchout
+  portwatchout = result.portwatchout
+
+
+
+  console.log(`[Version: ${packageJson.version}]`)
+  console.log(`[Last Used Config Settings]\n\nOSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nWatchout (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
+
+
+  let input = document.getElementById("input1")
+  input.value = iposc
+
+  let input2 = document.getElementById("input2")
+  input2.value = portosc
+
+  let input3 = document.getElementById("input3")
+  input3.value = ipwatchout
+
+  let input4 = document.getElementById("input4")
+  input4.value = portwatchout
+
+}
+
+const isRunOnce = async () => {
+  //const result = await window.electron.getIsRunOnce()
+  //console.log(isRunOnce1)
+  if (isRunOnce1){
+    isRunOnce1 = false
+    getConfigDefaults()
+  }
+}
+isRunOnce()
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  // window.electron.ipcRenderer.send('ping', 'ping')
+
+
+  setTextConnect(textConnected = "Connecting...")
+  const configData = {iposc, portosc, ipwatchout, portwatchout}
+  console.log("[OSC CONNECTED]\n[OSC SERVER RUNNING...]")
+  //console.log(configData)
+  const result = await window.electron.sendConfig(configData)
+  //console.log(result)
+
+}
+
+
+
+  return(
+    <div className='body1'>
+      <form onSubmit={
+        handleSubmit
+        }>
+        <Box
+      //component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 4, width: '21ch', },
+        tabSize: 2
+      }}
+      noValidate
+      autoComplete="off"
+      />
+      <div>
+      <h2 className='App-header'>OSC (In)</h2>
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+      size="small"
+      margin="dense"
+        id="input1"
+        label="IP"w
+        defaultValue={oscIpIn}
+        onChange={(e) => setOscIpIn(
+          e.target.value,
+          iposc = e.target.value,
+          //console.log(e)
+        )}
+        />
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+        id="input2"
+        size="small"
+        margin='dense'
+        label="Port"
+        defaultValue={oscPortIn}
+        onChange={(e) => setOscPortIn(
+          portosc = e.target.value,
+          e.target.value
+          )}
+        />
+
+        <div>
+        {/* {SimpleAccordion() } */}
+        {RecipeReviewCard()}
+      </div>
+
+      <br></br>
+      <h2 className='App-header'>Watchout (Out)</h2>
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+        margin="dense"
+        id="input3"
+        label="IP"
+        size="small"
+        defaultValue={watchoutIpOut}
+        onChange={(e) => setWatchoutIpOut(
+          e.target.value,
+          ipwatchout = e.target.value
+          )}
+        />
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+        margin="dense"
+        id="input4"
+        label="Port"
+        size="small"
+        defaultValue={watchoutPortOut}
+        onChange={(e) => setWatchoutPortOut(
+          e.target.value,
+          portwatchout = e.target.value
+          )}
+      />
+
+      <Button sx={{
+        position:'absolute',
+        bottom:"0",
+        right:"0",
+        height:60
+        }} fullWidth variant='contained'color={buttonConnected} type='submit'>{textConnected}</Button>
+      </div>
+      </form>
+    </div>
+  )
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
