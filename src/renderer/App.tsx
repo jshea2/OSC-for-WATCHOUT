4ff96097e077600f1644f00c5f8a2b39de4b6c6f
jshea2 <joe.daniel.shea@gmail.com>
2022-10-26 17:31:19 -0700

    modified:   assets/icon.icns 	modified:   assets/icon.ico 	modified:   assets/icon.png 	modified:   assets/icons/1024x1024.png 	modified:   assets/icons/128x128.png 	modified:   assets/icons/16x16.png 	modified:   assets/icons/24x24.png 	modified:   assets/icons/256x256.png 	modified:   assets/icons/32x32.png 	modified:   assets/icons/48x48.png 	modified:   assets/icons/512x512.png 	modified:   assets/icons/64x64.png 	deleted:    assets/icons/96x96.png 	modified:   config.json 	modified:   data/config.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main/main.ts 	modified:   src/renderer/App.tsx 	modified:   src/renderer/index.ejs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,6 +7,9 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import RecipeReviewCard from './ConsoleCollapse';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 let packageJson = require("../../package.json");
 
 let isRunOnce1 = true
@@ -14,8 +17,12 @@ let isRunOnce1 = true
 
 let iposc
 let portosc
+let iposcout
+let portoscout
 let ipwatchout
 let portwatchout
+let oscinenabled
+let oscoutenabled
 let buttonConnected
 let textConnected = "Connect"
 
@@ -24,10 +31,24 @@ const Hello = () => {
 
 let [oscIpIn, setOscIpIn] = useState("");
 let [oscPortIn, setOscPortIn] = useState("");
+let [oscIpOut, setOscIpOut] = useState("");
+let [oscPortOut, setOscPortOut] = useState("");
 let [watchoutIpOut, setWatchoutIpOut] = useState("");
 let [watchoutPortOut, setWatchoutPortOut] = useState("");
 let [connected, setConnected] = useState("")
 let [_, setTextConnect] = useState("Connect")
+
+const [checked, setChecked] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+const [checke, setChecke] = React.useState("");
+
+  const handleChang = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecke(event.target.checked);
+  };
 
 // const openDev = () => {
 //   window.electron.console("Hi")
@@ -45,25 +66,30 @@ window.electron.ipcRenderer.on('woconnected', (event, arg) => {
   //console.log(arg)
   if (arg == true){
     setConnected(buttonConnected = "success")
-  setTextConnect(textConnected = "Connected")
+    setTextConnect(textConnected = "Connected")
 } else {
   setConnected(buttonConnected = "error")
   setTextConnect(textConnected = "Disconnected")
   }
-  console.log(`OSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nWatchout (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
+  console.log(`OSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nOSC (Out)\nIP: ${iposcout} \nPort: ${portoscout}\n\nWebsocket (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
 })
 
 const getConfigDefaults = async () => {
   const result = await electron.getConfig()
   iposc = result.iposc
   portosc = result.portosc
+  iposcout = result.iposcout
+  portoscout = result.portoscout
   ipwatchout = result.ipwatchout
   portwatchout = result.portwatchout
-
+  oscinenabled = result.oscinenabled
+  setChecked(oscinenabled)
+  oscoutenabled = result.oscoutenabled
+  setChecke(oscoutenabled)
 
 
   console.log(`[Version: ${packageJson.version}]`)
-  console.log(`[Last Used Config Settings]\n\nOSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nWatchout (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
+  console.log(`[Last Used Config Settings]\n\nOSC (In)\nIP: ${iposc} \nPort: ${portosc}\n\nOSC (Out)\nIP: ${iposcout} \nPort: ${portoscout}\n\nWebsocket (Out)\nIP: ${ipwatchout}\nPort: ${portwatchout}`)
 
 
   let input = document.getElementById("input1")
@@ -72,11 +98,22 @@ const getConfigDefaults = async () => {
   let input2 = document.getElementById("input2")
   input2.value = portosc
 
+  let input5 = document.getElementById("input5")
+  input5.value = iposcout
+
+  let input6 = document.getElementById("input6")
+  input6.value = portoscout
+
   let input3 = document.getElementById("input3")
   input3.value = ipwatchout
 
   let input4 = document.getElementById("input4")
   input4.value = portwatchout
+
+  oscinenabled = checked
+
+  oscoutenabled = checke
+
 
 }
 
@@ -96,9 +133,14 @@ const handleSubmit = async (e) => {
 
   // window.electron.ipcRenderer.send('ping', 'ping')
 
+  if(textConnected == "Connected"){
+    return
+  }
 
+  oscinenabled = checked
+  oscoutenabled = checke
   setTextConnect(textConnected = "Connecting...")
-  const configData = {iposc, portosc, ipwatchout, portwatchout}
+  const configData = {iposc, portosc, iposcout, portoscout, ipwatchout, portwatchout, oscinenabled, oscoutenabled}
   console.log("[OSC CONNECTED]\n[OSC SERVER RUNNING...]")
   //console.log(configData)
   const result = await window.electron.sendConfig(configData)
@@ -123,7 +165,11 @@ const handleSubmit = async (e) => {
       autoComplete="off"
       />
       <div>
-      <h2 className='App-header'>OSC (In)</h2>
+      <FormGroup>
+      <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange}/>} label="OSC (IN)" />
+      </FormGroup>
+      {/* <h2 className='App-header'>OSC (In)</h2> */}
+
       <TextField
       sx={{
         input: { color: 'white'},
@@ -158,13 +204,52 @@ const handleSubmit = async (e) => {
           )}
         />
 
+
+      <FormGroup>
+      <FormControlLabel control={<Checkbox checked={checke} onChange={handleChang}/>} label="OSC (OUT)"/>
+      </FormGroup>
+
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+      size="small"
+      margin="dense"
+        id="input5"
+        label="IP"w
+        defaultValue={oscIpOut}
+        onChange={(e) => setOscIpOut(
+          e.target.value,
+          iposcout = e.target.value,
+          //console.log(e)
+        )}
+        />
+      <TextField
+      sx={{
+        input: { color: 'white'},
+        fieldSet: { borderColor: 'white'},
+        label: { color: 'white'}
+      }}
+        id="input6"
+        size="small"
+        margin='dense'
+        label="Port"
+        defaultValue={oscPortOut}
+        onChange={(e) => setOscPortOut(
+          portoscout = e.target.value,
+          e.target.value
+          )}
+        />
+
         <div>
         {/* {SimpleAccordion() } */}
         {RecipeReviewCard()}
       </div>
 
       <br></br>
-      <h2 className='App-header'>Watchout (Out)</h2>
+      <h2 className='App-header'>Websocket (Out)</h2>
       <TextField
       sx={{
         input: { color: 'white'},
